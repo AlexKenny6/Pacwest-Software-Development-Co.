465ee701e0b5eeb4f7a1338f5c1380983bcfffe7
Alex kenny <33143013+AlexKenny6@users.noreply.github.com>
2018-11-04 16:39:31 -0800

    Footer PHP File (Updated on 11/4/18)
    This is the footer code for every page

---
+++ Resources/footer.php
@@ -0,0 +1,51 @@
+<!--Portfolio-->
+    <h2><big>Portfolio</big></h2>
+    <section class="portfolio">
+        <a href="https://myohmymsblog.me" data-size="medium" target="_blank"><img src="img/Myohmymsblogphoto.jpg" alt=""></a>
+        <a href="https://www.nerdcoretoys.com" data-size="medium" target="_blank"><img src="img/NerdcoreHP.png" alt=""></a>
+        <a href="http://alexpkenny.com" data-size="medium" target="_blank"><img src="img/AlexKennyWebsiteTwo.png" alt=""></a>
+    </section>
+
+    <footer id="footer">
+        <section class="boxes">
+            <div class="box">
+                <h3>About<hr></h3>
+                <ul>
+                    <li>We Are a Software Development Company Who Takes The Time To Create a Product That You Should Be Bragging About To Everyone</li>
+                    <br>
+                    <li><a href="mailto:contact@pacwestsoftware.com"><i class="fas fa-envelope"></i> Send Us an Email</a></li>
+                    <li><i class="fas fa-map-marked"></i> <i>Everett, WA, USA</i></li>
+                </ul>
+            </div>
+            <div class="box">
+                <h3>Services<hr></h3>
+                <ul>
+                    <li><a href="web-dev.php"><i class="fas fa-desktop"></i> Web Development</a></li>
+                    <li><a href="ios-dev.php"><i class="fas fa-mobile-alt"></i> iOS Development</a></li>
+                    <li><a href="hosting.php"><i class="fas fa-home"></i> Hosting/Domain</a></li>
+                    <li><a href="seo-ga.php"><i class="fas fa-chart-line"></i> SEO/Google Analytics</a></li>
+                    <br>
+                    <li style="color: black">&copy; 2018, Pacwest Software Development LLC, <br>All Rights Reserved</li>
+                    <!--<li><a href="terms.html">Terms and Conditions</a></li>-->
+                </ul>
+            </div>
+            <div class="box">
+                <h3>More<hr></h3>
+                <ul>
+                    <li><a href="contact.php"><i class="fas fa-phone"></i> Contact Us</a></li>
+                    <li><a href="blog.php"><i class="fab fa-blogger"></i> Blog</a></li>
+                    <li><a href="qa.php"><i class="fas fa-question"></i> FAQ</a></li>
+                </ul>
+                <h3>Social Media<hr></h3>
+                <li id="li" align="center">
+                    <a href="https://www.instagram.com/pacwestsoftware/"><i class="fab fa-instagram"></i></a>
+                    <a href="https://www.facebook.com/pacwestsoftware/"><i class="fab fa-facebook-square"></i></a>
+                    <a href="https://twitter.com/PacwestSoftware"><i class="fab fa-twitter"></i></a>
+                </li>
+            </div>
+        </section>
+        <script src="js/main.js"></script>
+    </footer>
+    <!-- Wrapper Ends -->
+</body>
+</html>

d38f0872319dd1f2ad75250d3ff97f59fb99befe
Alex kenny <33143013+AlexKenny6@users.noreply.github.com>
2018-11-04 16:26:40 -0800

    SEO and Google Analytics Updated On (11/4/18)
    This page is about the SEO and Google Analytics marketing we do for each website

---
+++ seo-ga.php
@@ -0,0 +1,51 @@
+<?php require_once("../resources/config.php");  ?>
+<?php include(TEMPLATE_FRONT . DS . "header.php");  ?>
+<section class="top-container">
+    <header class="showcase-seo">
+        <h1>SEO/Google Analytics</h1>
+        <p>Wanting to move away from hosting companies who charge a lot? We can help you with that problem!</p>
+    </header>
+    <?php include(TEMPLATE_FRONT . DS . "side_nav.php");  ?>
+</section>
+<!--Info-->
+<section class="info-seo">
+    <div>
+        <h2>What we can do for you</h2>
+        <p>Hosting and Domain services come standard with every pricing plan we offer!</p>
+    </div>
+</section>
+<!--Boxes-->
+<section class="boxes-seo">
+    <div class="box">
+        <i class="fas fa-search fa-4x"></i>
+        <h3>Search Engine Optimization (SEO)</h3>
+        <p><i class="fas fa-check"></i> We will take the steps to make sure your website is compatible with Search Engines today like Google.</p>
+        <p><i class="fas fa-check"></i> We will help you rank higher on search charts.</p>
+    </div>
+    <div class="box">
+        <i class="fas fa-chart-line fa-4x"></i>
+        <h3>Google Analytics</h3>
+        <p><i class="fas fa-check"></i> By implementing Google Analytics, you will be able to track visitor data on your website.</p>
+        <p><i class="fas fa-check"></i> This information will be able to help you make smart business decisions.</p>
+    </div>
+</section>
+<!--Info #2-->
+<section class="info-seo">
+    <div>
+        <h2>Search Engine Optimization (SEO)</h2>
+        <ul>
+            <li><i class="fas fa-search"></i> A select amount of keyword that will focus on your topic.</li>
+            <li><i class="fas fa-search"></i> Information or articles that stay on topic and are high quality.</li>
+            <li><i class="fas fa-search"></i> Having social media accounts helps in seo and ranking.</li>
+        </ul>
+    </div>
+    <br>
+    <div>
+        <h2>Google Analytics</h2>
+        <ul>
+            <li><i class="fas fa-chart-line"></i> Keeping track of the amount of visitors to your website.</li>
+            <li><i class="fas fa-chart-line"></i> Helps in identifing where your auidence is located.</li>
+        </ul>
+    </div>
+</section>
+<?php include(TEMPLATE_FRONT . DS . "footer.php");  ?>

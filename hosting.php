28cb26cd5cc0f898228b97766c9f85b17e12915e
Alex kenny <33143013+AlexKenny6@users.noreply.github.com>
2018-11-04 16:25:32 -0800

    Website Hosting Updated On (11/4/18)
    This page is about the kind of website hosting we offer

---
+++ hosting.php
@@ -0,0 +1,63 @@
+<?php require_once("../resources/config.php");  ?>
+<?php include(TEMPLATE_FRONT . DS . "header.php");  ?>
+<section class="top-container">
+    <header class="showcase-hosting">
+        <h1>Hosting/Domain</h1>
+        <p>Wanting to move away from hosting companies who charge a lot? We can help you with that problem!</p>
+    </header>
+    <?php include(TEMPLATE_FRONT . DS . "side_nav.php");  ?>
+</section>
+<!--Info-->
+<section class="info-hosting">
+    <div>
+        <h2>What we can do for you</h2>
+        <p>Hosting and Domain services come standard with every pricing plan we offer!</p>
+    </div>
+</section>
+<!--Boxes-->
+<section class="boxes-hosting">
+    <div class="box">
+        <i class="fas fa-archive fa-4x"></i>
+        <h3>Storage</h3>
+        <p><i class="fas fa-check"></i> We have storage to keep every piece of data that is created on your website or app.</p>
+        <p><i class="fas fa-check"></i> SSD storage will keep your data moving very fa-desktop.</p>
+    </div>
+    <div class="box">
+        <i class="fas fa-lock fa-4x"></i>
+        <h3>Security</h3>
+        <p><i class="fas fa-check"></i> We keep your privacy a top priority and with the AWS servers on our side, your data will be secure.</p>
+        <p><i class="fas fa-check"></i> All servers have top of the line security with firewalls in place.</p>
+    </div>
+    <div class="box">
+        <i class="fas fa-home fa-4x"></i>
+        <h3>Domain Security</h3>
+        <p><i class="fas fa-check"></i> We will have Whois domain protection as a standard for website security.</p>
+        <p><i class="fas fa-check"></i> SSL Certificates will be used on your website keeping it safe from harm.</p>
+    </div>
+</section>
+<!--Info #2-->
+<section class="info-hosting">
+    <div>
+        <br><br>
+        <h2>Hosting</h2>
+        <ul>
+            <li><i class="fas fa-home"></i> Storage options range in amounts of gigabytes or GB.</li>
+            <li><i class="fas fa-home"></i> We will take the files we have created for the website and use a version of CPanel to upload them to the internet.</li>
+            <li><i class="fas fa-home"></i> Your website and app files will be stored on a Virtual Private Server or (VPS).</li>
+            <li><i class="fas fa-home"></i> Hosting security will be far greater than on an shared server.</li>
+            <li><i class="fas fa-home"></i> You can either buy a domain name or we can buy one for you.</li>
+        </ul>
+    </div>
+    <br>
+    <div>
+        <br><br>
+        <h2>Domain Security</h2>
+        <ul>
+            <li><i class="fas fa-lock"></i> Websites that are for personal use or for a business will have website security.</li>
+            <li><i class="fas fa-lock"></i> A SSL Certificate will give you a "lock" icon next to your domain.</li>
+            <li><i class="fas fa-lock"></i> SSL Certificates are up to a 256 bit encryption and from a key legnth of 2048.</li>
+            <li><i class="fas fa-lock"></i> Your website will have Whois protection allowing you to avoid scammers calling your cell phone all the time.</li>
+        </ul>
+    </div>
+</section>
+<?php include(TEMPLATE_FRONT . DS . "footer.php");  ?>

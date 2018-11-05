c9c46622d57119e1e92d5c2f520442bbb7bbae27
Alex kenny <33143013+AlexKenny6@users.noreply.github.com>
2018-11-04 16:22:33 -0800

    iOS Dev Updated On (11/4/18)
    iOS dev page about our iOS App design and development service

---
+++ ios-dev.php
@@ -0,0 +1,81 @@
+<?php require_once("../resources/config.php");  ?>
+<?php include(TEMPLATE_FRONT . DS . "header.php");  ?>
+<section class="top-container">
+    <header class="showcase-ios">
+        <h1>iOS App Design and Development</h1>
+        <p>Thinking of expanding your business to the smartphone app world? You came to the right place!</p>
+    </header>
+    <?php include(TEMPLATE_FRONT . DS . "side_nav.php");  ?>
+</section>
+<!--Info-->
+<section class="info-ios">
+    <div>
+        <h2>What we can do for you</h2>
+        <p>Every single item listed here comes standard with every pricing plan we have. Lets see what we do!</p>
+    </div>
+</section>
+<!--Boxes-->
+<section class="boxes-ios">
+    <div class="box">
+        <i class="fas fa-desktop fa-4x"></i>
+        <h3>Designs</h3>
+        <p><i class="fas fa-check"></i> We are going to collaborate and sit down to discuss what features your app will have.</p>
+        <p><i class="fas fa-check"></i> We want to have them same mindset on what will occur.</p>
+    </div>
+    <div class="box">
+        <i class="fas fa-mobile-alt fa-4x"></i>
+        <h3>Lots of Coding</h3>
+        <p><i class="fas fa-check"></i> After the meetings, we will develop the app for you with XCode.</p>
+        <p><i class="fas fa-check"></i> We will spend day and night to create your site.</p>
+    </div>
+    <div class="box">
+        <i class="fas fa-home fa-4x"></i>
+        <h3>Testing</h3>
+        <p><i class="fas fa-check"></i> Tests will take place to test the code and make sure everything is working smoothly.</p>
+        <p><i class="fas fa-check"></i> We will use many Apple products ranging from older iPhones to the new iPhone X.</p>
+        <p><i class="fas fa-check"></i> We will use iPads to test your app as well.</p>
+    </div>
+    <div class="box">
+        <i class="fas fa-archive fa-4x"></i>
+        <h3>Back Ups</h3>
+        <p><i class="fas fa-check"></i> We will back up your website files and server data to a physical hard drive for security measures.</p>
+        <p><i class="fas fa-check"></i> We will take care of the backup files and treat them like the priceless art in the Vatican.</p>
+    </div>
+    <div class="box">
+        <i class="fab fa-app-store-ios fa-4x"></i>
+        <h3>Uploading to the App Store</h3>
+        <p><i class="fas fa-check"></i> We will work with the App Store Review Team to get your app on the App Store.</p>
+    </div>
+    <div class="box">
+        <i class="fas fa-users fa-4x"></i>
+        <h3>Customer Support</h3>
+        <p><i class="fas fa-check"></i> We will be with you every step of the way and make sure every problem is handled.</p>
+        <p><i class="fas fa-check"></i> We are always available for anything that you may need, regardless of priority status.</p>
+    </div>
+</section>
+<!--Info #2--> 
+<section class="info-ios">
+    <div>
+        <h2>Lets start with the designing of the App!</h2>
+        <p>We will use pen and paper to get a basic overview of the app.</p>
+        <ul>
+            <li><i class="fas fa-mobile-alt"></i> Listing the features you want.</li>
+            <li><i class="fas fa-mobile-alt"></i> Drawing the different pages you want.</li>
+            <li><i class="fas fa-mobile-alt"></i> Discussing how each screen connects to other screens with buttons.</li>
+            <li><i class="fas fa-mobile-alt"></i> Ideas on what each screen will say.</li>
+        </ul>
+    </div>
+    <br>
+    <div>
+        <h2>After we have completed the designing stage, we will begin to code (our favorite part).</h2>
+        <p>We will start by opening up XCode and starting a file in the language Swift. Objective C and Swift are the only two languages that are used to make iOS apps.</p>
+        <p>Let's take a look at an example file:</p>
+        <br><br>
+        <img src="img/swift-coding.png" alt="Swift Coding Picture">
+        <br><br>
+        <p>You can see in the picture that we are using swift to create an app. You can find the rest of the app on our GitHub page.</p>
+        <br>
+        <p>After the development phase is over, we will work with the App Store Review Team to get the app live and ready for download.</p>
+    </div>
+</section>
+<?php include(TEMPLATE_FRONT . DS . "footer.php");  ?>

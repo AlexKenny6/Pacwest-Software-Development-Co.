302b38ef8db498e8b187f30992dbf85c2db6e4be
Alex kenny <33143013+AlexKenny6@users.noreply.github.com>
2018-11-04 16:32:27 -0800

    Header PHP File (Updated on 11/4/18)
    This is the header for every page

---
+++ header.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link rel="stylesheet" href="css/main.css" type="text/css">
+        <link rel="icon" href="img/favicon.ico" type="image/x-icon" />
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+        <meta name="description" content="Pacwest Software Development LLC">
+        <meta name="author" content="Pacwest Software Development LLC">
+        <title>Pacwest Software Dev LLC</title>
+
+        <!-- Favicon -->
+        <link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="manifest" href="/site.webmanifest">
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#1db954">
+        <meta name="msapplication-TileColor" content="#1db954">
+        <meta name="theme-color" content="#1db954">
+        <!-- End of Favicon -->
+
+
+        <!-- Google analytics tracking stuff -->
+            <!-- Global site tag (gtag.js) - Google Analytics -->
+            <script async src="https://www.googletagmanager.com/gtag/js?id=UA-UA-128544159-1"></script>
+            <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'UA-128544159-1');
+            gtag('set', {'WebsiteViewData': '166440886'}); // Set the user ID using signed-in user_id.
+            </script>
+
+             <!-- Global site tag (gtag.js) - Google Analytics -->
+             <script async src="https://www.googletagmanager.com/gtag/js?id=UA-111264251-1"></script>
+            <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'UA-111264251-1');
+            </script>
+        <!-- End of Google analytics tracking -->
+
+    </head>
+    <body href="#myPage" data-spy="scroll" data-offset="60"> 
+        <div class="wrapper">
+            <nav class="main-nav">
+               <?php include(TEMPLATE_FRONT . DS . "top_nav.php");  ?>
+            </nav>

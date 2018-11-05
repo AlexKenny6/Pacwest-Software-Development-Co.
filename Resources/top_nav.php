1e472fd15da45cbdf4e4c5916870602428f62a50
Alex kenny <33143013+AlexKenny6@users.noreply.github.com>
2018-11-04 16:40:25 -0800

    Top-Nav PHP File (Updated on 11/4/18)
    This is the top navigation bar for every page

---
+++ Resources/top_nav.php
@@ -0,0 +1,8 @@
+ <ul>
+    <li><a href="index.php">Home</a></li>
+    <li><a href="about.php">About</a></li>
+    <li><a href="services.php">Services</a></li>
+    <li><a href="pricing.php">Pricing</a></li>
+    <li><a href="contact.php">Contact</a></li>
+    <li><a href="qa.php">FAQ</a></li>
+</ul>

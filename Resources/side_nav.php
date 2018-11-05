85f2ba88242bd02895db0a3eeac6ea975cf04bf3
Alex kenny <33143013+AlexKenny6@users.noreply.github.com>
2018-11-04 16:38:58 -0800

    Side-Nav PHP File (Updated on 11/4/18)
    This is the side navigation code for every page

---
+++ Resources/side_nav.php
@@ -0,0 +1,8 @@
+<div class="top-box top-box-a">
+    <h4>Recent Posts</h4>
+    <a href="blog.php" class="btn">Click Here</a>
+</div>
+<div class="top-box top-box-b">
+    <h4>Github Account</h4>
+    <a href="https://github.com/AlexKenny6" class="btn">Click Here</a>
+</div>

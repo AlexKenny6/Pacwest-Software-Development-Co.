6763007e5b74c11066771388813942f28e661a9d
Alex kenny <33143013+AlexKenny6@users.noreply.github.com>
2018-11-04 16:27:47 -0800

    Our Blow Updated On (11/4/18)
    This page has our blog on it where we post articles about the company and its activity

---
+++ blog.php
@@ -0,0 +1,58 @@
+<?php require_once("../resources/config.php");  ?>
+<?php include(TEMPLATE_FRONT . DS . "header.php");  ?>
+<section class="top-container">
+    <header class="showcase-blog">
+        <h1>Our Blog</h1>
+        <p>Hello, we want to provide you with useful information about web development, iOS app development, and any other tech news!</p>
+    </header>
+    <?php include(TEMPLATE_FRONT . DS . "side_nav.php");  ?>
+</section>
+<!--Article Boxes-->
+<h2><big>Recent Articles</big></h2>
+<section class="boxes-blog">
+    <div class="box-blog">
+        <h2>Instagram Update</h2>
+        <h3>October 7th, 2018</h3>
+        <p>
+            If you guys have't gone to our <a href="https://www.instagram.com/pacwestsoftware/" data-size="medium" target="_blank"><i>Instagram account</i></a>, we are still posting everyday as we are doing the 100 Days of Code challenge.
+            We are having a ton of fun posting everyday and we will continue to do so for a long time :).
+            <br><br>
+            We also connected our Facebook and Twitter accounts to the Instagram account so everytime we post to Insta, they will appear on <a href="https://www.facebook.com/pacwestsoftware/" data-size="medium" target="_blank"><i>Facebook</i></a> and <a href="https://twitter.com/PacwestSoftware" data-size="medium" target="_blank"><i>Twitter</i></a>!
+        </p>
+    </div>
+</section>
+<section class="boxes-blog">
+    <div class="box-blog">
+        <h2>Apple Event and Early September Update</h2>
+        <h3>September 15th, 2018</h3>
+        <p>
+            Did you guys hear the news? Apple will be launching three new iPhones this month that have advanced features like the A12 processor and the OLED screen!
+            We decided to buy the iPhone XS to help us in testing our iOS apps that are in production. <br><br>
+            Speaking of apps, if you guys want to watch our ios app development jounrey, go check out our Instagram <a href="https://www.instagram.com/pacwestsoftware/" data-size="medium" target="_blank">here</a> as we are posting a lot of pictures about it.
+            For example we posted a picture of the Adobe XD prototype we developed. It is an exciting process and we are working everyday to launch this app soon!
+        </p>
+    </div>
+</section>
+<section class="boxes-blog">
+    <div class="box-blog">
+        <h2>Our Social Media</h2>
+        <h3>September 7th, 2018</h3>
+        <p>
+            Hey everyone, we are now on Instagram at <i><a href="https://www.instagram.com/pacwestsoftware/" data-size="medium" target="_blank">@Pacwestsoftware</a></i> and on Twitter <i><a href="https://twitter.com/PacwestSoftware" data-size="medium" target="_blank">@Pacwestsoftware</a></i>. 
+            Go check out our Instagram, we are doing the 100 Days Of Code Challenge where we will be posting everyday about our coding journey.
+            Every post we make will have the hashtags <b>#Pacwestsoftware</b> and <b>#100DaysOfCode</b> on it!
+            The streak of posts will definitely continue after the 100 days.
+        </p>
+    </div>
+</section>
+<section class="boxes-blog">
+    <div class="box-blog">
+        <h2>Website Update</h2>
+        <h3>Aug 9th, 2018</h3>
+        <p>
+            Hello all! We just released a new update to our website. The first version of the website was a way to get our name out there however change was needed. 
+            In the past week weeks, we have been upgrading the website to include more information that is relevant and helpful.
+        </p>
+    </div>
+</section>
+<?php include(TEMPLATE_FRONT . DS . "footer.php");  ?>
